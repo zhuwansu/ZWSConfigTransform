6d04f5616aea38c331533efd55c685aac26f8902
皖苏 朱 <18410180951@163.com>
2018-07-06 18:04:18 +0800

    添加解决方案

---
+++ ZWS.ConfigTransform/ZWS.ConfigTransform/ConfigTransformer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ZWS.ConfigTransform
+{
+    public static class ConfigTransformer
+    {
+        public static void Transform(string configName, string sourceDir, string targetFile)
+        {
+
+            var baseConfigNode = XElement.Load(targetFile);
+            var targetConfigNode = XElement.Parse(File.ReadAllText(sourceDir + $"{configName}.config"));
+
+            ReplaceAddElements("appSettings", "key", baseConfigNode, targetConfigNode);
+
+            ReplaceAddElements("connectionStrings", "name", baseConfigNode, targetConfigNode);
+
+            baseConfigNode.Save(targetFile);
+        }
+
+        static void ReplaceAddElements(string fElement, string matchAttrName, XElement baseConfigNode, XElement targetConfigNode)
+        {
+            var baseConfig = baseConfigNode.Element(fElement).Elements("add").ToArray();
+            var targetConfig = targetConfigNode.Element(fElement).Elements("add").ToArray();
+
+            var temp = baseConfig.ToDictionary((m) =>
+            {
+                return m.Attribute(matchAttrName).Value;
+            });
+
+            for (int j = 0; j < targetConfig.Length; j++)
+            {
+                var targetItem = targetConfig[j];
+                var targetKey = targetItem.Attribute(matchAttrName).Value;
+                temp[targetKey].ReplaceWith(targetItem);
+            }
+        }
+    }
+}
---
+++ ZWS.ConfigTransform/ZWS.ConfigTransform/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZWS.ConfigTransform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configName = args[0];
+            var sourceDir = args[1];
+            var targetFile = args[2];
+
+            ConfigTransformer.Transform(configName, sourceDir, targetFile);
+        }
+    }
+}
